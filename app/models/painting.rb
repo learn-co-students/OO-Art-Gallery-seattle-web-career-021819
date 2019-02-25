4f03891043bc28d1f5cc5ecd7881c29b83eb81a0
github username <github email address>
2019-02-24 21:34:28 -0800

    I think I got it working?

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,26 @@
 class Painting
 
-  attr_reader :title, :price
+  attr_reader :title, :price, :artist
 
   @@all = []
 
-  def initialize(title, price)
+  def initialize(title, price, artist)
     @title = title
     @price = price
+    @@all << self
+    @artist = artist
   end
+
+def self.all
+  @@all
+end
+
+def self.total_price
+  prices = []
+   @@all.each do |pnt|
+     prices << pnt.price
+   end
+   return prices.reduce(:+)
+end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,20 @@
 require_relative '../config/environment.rb'
 
+monet = Artist.new("Monet", 20)
+steve = Artist.new("Steve", 1)
+picasso = Artist.new("picasso", 12)
 
+sunflowers = Painting.new("Sunflowers", 10000, monet)
+tigers = Painting.new("Tigers", 5, steve)
+vase = Painting.new("Vase of Flowers", 300, picasso)
+person = Painting.new("Weird Dude", 5000, picasso)
 
+louvre = Gallery.new("Louvre", "Paris")
+met = Gallery.new("Metropolitan Museum of Art", "New York")
+louvre.art << tigers
+louvre.art << vase
+louvre.art << sunflowers
+met.art << person
 
 binding.pry
 
