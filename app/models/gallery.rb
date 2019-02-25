752faa9007c61823e2f0e9781b3e1b17eb2b4a70
github username <github email address>
2019-02-25 13:39:58 -0800

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,35 @@
 class Gallery
-
   attr_reader :name, :city
+  @@all = []
 
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
   end
 
+  # Returns an array of all paintings in a gallery
+  def paintings
+    Painting.all.select {|painting| painting.gallery == self}
+  end
+
+  # Returns an array of all artists that have a painting in a gallery
+  def artists
+    self.paintings.map {|painting| painting.artist}
+  end
+
+  # Returns an array of the names of all artists
+  # that have a painting in a gallery
+  def artist_names
+    self.artists.map {|artist| artist.name}
+  end
+
+  # Returns an instance of the most expensive painting in a gallery
+  def most_expensive_painting
+    self.paintings.max_by {|painting| painting.price }
+  end
+
+  def self.all
+    @@all
+  end
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,23 @@
 class Painting
-
   attr_reader :title, :price
+  attr_accessor :artist, :gallery
 
   @@all = []
 
-  def initialize(title, price)
+  def initialize(title, artist, price, gallery)
     @title = title
+    @artist = artist
     @price = price
+    @gallery = gallery
+    @@all << self
   end
 
+  # Returns an integer that is the total price of all paintings
+  def total_price
+    self.all.inject {|sum, painting| sum+painting.price}
+  end
+
+  def self.all
+    @@all
+  end
 end
