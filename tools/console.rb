8d5eac848f4c58ead6a298c721c13d4f89f84386
github username <github email address>
2019-02-24 19:02:09 -0800

    Completed lab w/ some basic test objects in console.rb

--- tools/console.rb
+++ tools/console.rb
@@ -1,5 +1,17 @@
 require_relative '../config/environment.rb'
 
+picasso=Artist.new("Picasso", 50)
+mucha=Artist.new("Mucha", 25)
+vangogh=Artist.new("Vincent VG", 5)
+
+met=Gallery.new("MET", "New York City")
+moma=Gallery.new("MoMA", "Manhattan")
+
+gurenica=Painting.new("Gurenica", 100, picasso, met)
+selfportrait=Painting.new("Self Portrait", 50, picasso, met)
+
+slav_epic=Painting.new("Slav Epic", 75, mucha, moma)
+starry_night=Painting.new("Starry Night", 500, vangogh, moma)
 
 
 
