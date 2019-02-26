3746c0464dba40566de35c9d0803ab8c752cdcb1
Reid Boudreau <rtboudreau@gmail.com>
2019-02-25 17:31:45 -0800

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,46 @@
 class Artist
 
   attr_reader :name, :years_experience
+  @@all = []
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select do |painting_v|
+      painting_v.artist == self
+    end
+  end
+
+  def galleries
+    paintings.map do |painting_v|
+      painting_v.gallery
+    end
+  end
+
+  def cities
+    galleries.map do |gallery_v|
+      gallery_v.city
+    end
+  end
+
+  def self.total_experience
+    self.all.reduce(0) do |sum, num|
+      sum + num.years_experience
+    end
+  end
+
+  def create_painting(title, price, gallery)
+
+  end
+
+
 
 end
